5460e42f52790ba65d8b77a006ff017ec882980d
Himalaya7087 <himalayasingh.hs@gmail.com>
2024-07-05 00:20:29 +0530

    jenkins

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -8,9 +8,6 @@ fi
 
 # Remove old Java files
 echo "Removing old Java files in /java-app..."
-rm -rf /java-app/*
+rm -rf /java-app/
 
-# Copy new JAR file to java-app directory
-echo "Copying new JAR file: project-0.1.8.jar to /java-app"
-cp project-0.1.8.jar /java-app/
 
